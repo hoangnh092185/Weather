ac232ce5047e708cd96b1ec61d69a9ce73907b59
Nhat Hoang <hoangnh092185@gmail.com>
2017-01-23 14:00:02 -0800

    Display map on full screen.

--- src/main.js
+++ src/main.js
@@ -1,37 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import MapView from 'react-native-maps';
-// import Api from './src/api';
+
 
 export default class main extends Component {
-  constructor(){
+  constructor() {
     super();
   }
-  render(){
-    // const { region } = this.props;
-    // console.log(region);
 
+  render() {
     return (
-        <MapView
-          onRegionChangeComplete={this.onRegionChangeComplete}
-          style={styles.map}>
-        </MapView>
+      <View style={styles.container}>
+      <MapView
+        style={ styles.map }
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      /></View>
     );
   }
-  onRegionChangeComplete(region){
-    console.log(region);
-  }
 }
-const styles = StyleSheet.create({
- map: {
-   flex: 1
- }
-});
+
+var styles = StyleSheet.create({
+  container: {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+
+
+   },
+   map: {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+   },
+ });
 
 AppRegistry.registerComponent('weather', () => Main);
